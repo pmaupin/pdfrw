99509cee268031bbfd2443ef08e8dd00fde28a54
pmaupin <pmaupin@bf5f1c66-d648-11de-a63d-2763023b5f98>
2010-02-15 05:54:30 +0000

    Major start on PdfReader() refactoring. Still needs some cleanup and comments.

--- branches/jython2.2.1/pdfrw/pdfreader.py
+++ branches/jython2.2.1/pdfrw/pdfreader.py
@@ -21,66 +21,64 @@ from pdfcompress import uncompress
 
 class PdfReader(PdfDict):
 
-    class unresolved:
-        # Used as a placeholder until we have an object.
+    class DeferredObject(object):
         pass
 
-    def deferred_object(self, objnum, gennum):
-        class Deferred(object):
-            pass
-        result = Deferred()
-        result.info = objnum, gennum
-        self.deferred.append(result)
-        return result
-
-    def expand_deferred(self):
-        missing = set()
-        for obj in list(self.deferred):
-            deferred_again, newobj = self.readindirect(*obj.info)
-            if deferred_again:
-                missing.add(obj.info)
-            else:
-                obj.parent[obj.index] = newobj
-        if missing:
-            print "Objects missing from PDF file: %s" % sorted(missing)
-
-    def readindirect(self, objnum, gennum):
+    def readindirect(self, objnum, gennum, Deferred=DeferredObject):
         ''' Read an indirect object.  If it has already
             been read, return it from the cache.
         '''
+        key = int(objnum), int(gennum)
+        result = self.indirect_objects.get(key)
+        if result is None:
+            result = Deferred()
+            result.key = key
+            result.usedby = []
+            self.indirect_objects[key] = result
+            self.deferred.add(result)
+        return isinstance(result, Deferred), result
 
-        def setobj(obj):
-            # Store the new object in the dictionary
-            # once we have its value
-            record[1] = obj
+    def readarray(self, source):
+        special = self.special
+        result = PdfArray()
 
-        def ordinary(source, setobj, obj):
-            # Deal with an ordinary (non-array, non-dict) object
-            setobj(obj)
-            return obj
+        for value in source:
+            if value == ']':
+                break
+            deferred = False
+            if value in special:
+                value = special[value](source)
+            elif value == 'R':
+                generation = result.pop()
+                deferred, value = self.readindirect(result.pop(), generation)
+            if deferred:
+                value.usedby.append((result, len(result)))
+            result.append(value)
+        return result
 
-        fdata, objnum, gennum = self.fdata, int(objnum), int(gennum)
-        try:
-            record = self.indirect_objects[fdata, objnum, gennum]
-        except KeyError:
-            return True, self.deferred_object(objnum, gennum)
-        if record[1] is not self.unresolved:
-            return False, record[1]
+    def readdict(self, source):
+        special = self.special
+        result = PdfDict()
 
-        # Read the object header and validate it
-        source = PdfTokens(fdata, record[0])
-        objid = source.multiple(3)
-        assert int(objid[0]) == objnum, objid
-        assert int(objid[1]) == gennum, objid
-        assert objid[2] == 'obj', objid
-
-        # Read the object, and call special code if it starts
-        # an array or dictionary
-        obj = source.next()
-        obj = self.special.get(obj, ordinary)(source, setobj, obj)
-        self.readstream(obj, source)
-        obj.indirect = True
-        return False, obj
+        tok = source.next()
+        while tok != '>>':
+            assert tok.startswith('/'), (tok, source.multiple(10))
+            key = tok
+            value = source.next()
+            deferred = False
+            if value in special:
+                value = special[value](source)
+                tok = source.next()
+            else:
+                tok = source.next()
+                if value.isdigit() and tok.isdigit():
+                    assert source.next() == 'R'
+                    deferred, value = self.readindirect(value, tok)
+                    tok = source.next()
+            if deferred:
+                value.usedby.append((result, key))
+            result[key] = value
+        return result
 
     def readstream(obj, source):
         ''' Read optional stream following a dictionary
@@ -100,59 +98,60 @@ class PdfReader(PdfDict):
             ch = fdata[floc]
         assert ch == '\n'
         startstream = floc + 1
-        endstream = startstream + int(obj.Length)
-        obj._stream = fdata[startstream:endstream]
-        source = PdfTokens(fdata, endstream)
-        endit = source.multiple(2)
-        assert endit == 'endstream endobj'.split(), endit
+        return startstream
     readstream = staticmethod(readstream)
 
-    def readarray(self, source, setobj=lambda x:None, original=None):
-        special = self.special
-        result = PdfArray()
-        setobj(result)
+    def expand_deferred(self):
+        deferredset = self.deferred
+        obj_offsets = self.obj_offsets
+        specialget = self.special.get
+        indirect_objects = self.indirect_objects
+        readstream = self.readstream
+        fdata = self.fdata
+        DeferredObject = self.DeferredObject
+        streams = []
 
-        for value in source:
-            if value == ']':
-                break
-            deferred = False
-            if value in special:
-                value = special[value](source)
-            elif value == 'R':
-                generation = result.pop()
-                deferred, value = self.readindirect(result.pop(), generation)
-            if deferred:
-                value.parent = result
-                value.index = len(result)
-            result.append(value)
-        return result
+        while deferredset:
+            deferred = deferredset.pop()
+            key = deferred.key
+            offset = obj_offsets[key]
 
-    def readdict(self, source, setobj=lambda x:None, original=None):
-        special = self.special
-        result = PdfDict()
-        setobj(result)
+            # Read the object header and validate it
+            objnum, gennum = key
+            source = PdfTokens(fdata, offset)
+            objid = source.multiple(3)
+            assert int(objid[0]) == objnum, objid
+            assert int(objid[1]) == gennum, objid
+            assert objid[2] == 'obj', objid
 
-        tok = source.next()
-        while tok != '>>':
-            assert tok.startswith('/'), (tok, source.multiple(10))
-            key = tok
-            value = source.next()
-            deferred = False
-            if value in special:
-                value = special[value](source)
-                tok = source.next()
-            else:
-                tok = source.next()
-                if value.isdigit() and tok.isdigit():
-                    assert source.next() == 'R'
-                    deferred, value = self.readindirect(value, tok)
-                    tok = source.next()
-            if deferred:
-                value.parent = result
-                value.index = key
-            result[key] = value
+            # Read the object, and call special code if it starts
+            # an array or dictionary
+            obj = source.next()
+            func = specialget(obj)
+            if func is not None:
+                obj = func(source)
+            startstream = readstream(obj, source)
+            obj.indirect = True
+            indirect_objects[key] = obj
+            deferred.value = obj
+            for parent, index in deferred.usedby:
+                parent[index] = obj
 
-        return result
+            if startstream is not None:
+                streams.append((obj, startstream))
+
+        for obj, startstream in streams:
+            endstream = startstream + int(obj.Length)
+            obj._stream = fdata[startstream:endstream]
+            source = PdfTokens(fdata, endstream)
+            endit = source.multiple(2)
+            assert endit == 'endstream endobj'.split(), endit
+
+        newdict = {}
+        for key, obj in self.iteritems():
+            if isinstance(obj, DeferredObject):
+                newdict[key] = obj.value
+        self.update(newdict)
 
     def readxref(fdata):
         startloc = fdata.rfind('startxref')
@@ -165,6 +164,8 @@ class PdfReader(PdfDict):
     readxref = staticmethod(readxref)
 
     def parsexref(self, source):
+        fdata = self.fdata
+        obj_offsets = self.obj_offsets
         tok = source.next()
         assert tok == 'xref', tok
         while 1:
@@ -176,9 +177,8 @@ class PdfReader(PdfDict):
                 offset = int(source.next())
                 generation = int(source.next())
                 if source.next() == 'n':
-                    objid = self.fdata, objnum, generation
-                    objval = [offset, self.unresolved]
-                    self.indirect_objects.setdefault(objid, objval)
+                    objid = objnum, generation
+                    obj_offsets.setdefault(objid, offset)
 
     pagename = PdfName.Page
     pagesname = PdfName.Pages
@@ -212,7 +212,8 @@ class PdfReader(PdfDict):
 
         self.private.indirect_objects = {}
         self.private.special = {'<<': self.readdict, '[': self.readarray}
-        self.private.deferred = deferred = []
+        self.private.deferred = deferred = set()
+        self.private.obj_offsets = {}
 
         startloc, source = self.readxref(fdata)
         while 1:
