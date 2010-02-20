d3e6a3ae1213fa7834055b8405030dbac3944b9c
fpieraut <fpieraut@bf5f1c66-d648-11de-a63d-2763023b5f98>
2010-02-20 05:49:46 +0000

    added OptionParser to example/watermark.py

--- trunk/examples/watermark.py
+++ trunk/examples/watermark.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-Simple example of watermarking using form xobjects.
+Simple example of watermarking using form xobjects (pdfrw).
 
 usage:   watermark.py my.pdf single_page.pdf
 
@@ -49,13 +49,66 @@ def fixpage(page, watermark):
     contents.append(IndirectPdfDict(stream='Q %s Do\n' % watermark_name))
     return page
 
-try:
-    inpfn, waterfn = sys.argv[1:]
-except:
-    raise SystemExit('Usage:  watermark.py <input.pdf> <single_page.pdf>')
+def watermark(input_fname, watermark_fname, output_fname=None):
+    outfn = output_fname or ('watermark.' + os.path.basename(input_fname))
+    w = pagexobj(PdfReader(watermark_fname, decompress=False).pages[0])
+    pages = PdfReader(input_fname, decompress=False).pages
+    PdfWriter().addpages([fixpage(x, w) for x in pages]).write(outfn)
+    return outfn
 
-watermark = pagexobj(PdfReader(waterfn, decompress=False).pages[0])
-outfn = 'watermark.' + os.path.basename(inpfn)
-pages = PdfReader(inpfn, decompress=False).pages
-
-PdfWriter().addpages([fixpage(x, watermark) for x in pages]).write(outfn)
+def fix_pdf(fname, watermark_fname, indir, outdir):
+    from os import mkdir, path
+    if not path.exists(outdir):
+        mkdir(outdir)
+    watermark = pagexobj(PdfReader(watermark_fname, decompress=False).pages[0])
+    trailer = PdfReader(path.join(indir, fname), decompress=False)
+    for page in trailer.pages:
+        fixpage(page, watermark)
+    PdfWriter().write(path.join(outdir, fname), trailer)
+    return len(trailer.pages)
+    
+def batch_watermark(pdfdir, watermark_fname, outputdir='tmp'):
+    import traceback
+    from glob import glob
+    from os import path
+    fnames=glob(pdfdir+"/*.pdf")
+    total_pages = 0
+    good_files = 0
+    
+    for fname in fnames:
+        fname = fname.replace(pdfdir+'/','')
+        try:
+            total_pages += fix_pdf(fname, watermark_fname, pdfdir, outputdir)
+            good_files += 1
+            print "%s OK" %fname
+        except Exception:
+            print "%s Failed miserably" %fname
+            print traceback.format_exc()[:2000]
+            #raise
+    
+    print "success %.2f%% %s pages" %((float(good_files)/len(fnames))*100, total_pages)
+    
+if __name__ == "__main__":
+    
+    from optparse import OptionParser
+    parser = OptionParser(description = __doc__)
+    parser.add_option('-i', dest='input_fname', help='file name to be watermarked (pdf)')
+    parser.add_option('-w', dest='watermark_fname', help='watermark file name (pdf)')
+    parser.add_option('-d', dest='pdfdir', help='watermark all pdf files in this directory')
+    parser.add_option('-o', dest='outdir', help='outputdir used with option -d', default='tmp')
+    options, args = parser.parse_args()
+    
+    if options.input_fname and options.watermark_fname:
+        watermark = pagexobj(PdfReader(options.watermark_fname, decompress=False).pages[0])
+        outfn = 'watermark.' + os.path.basename(options.input_fname)
+        pages = PdfReader(options.input_fname, decompress=False).pages
+        
+        PdfWriter().addpages([fixpage(x, watermark) for x in pages]).write(outfn)
+    
+    elif options.pdfdir and options.watermark_fname:
+        batch_watermark(options.pdfdir, options.watermark_fname, options.outdir)
+    
+    else:
+        parser.print_help()
+        
+        
