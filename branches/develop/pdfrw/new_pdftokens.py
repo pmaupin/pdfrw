43d1d359c4dbd0a080d152568fe48286656e13b8
pmaupin <pmaupin@bf5f1c66-d648-11de-a63d-2763023b5f98>
2012-09-05 05:13:22 +0000

    Saving work in progress on new tokenizer

--- branches/develop/pdfrw/new_pdftokens.py
+++ branches/develop/pdfrw/new_pdftokens.py
@@ -11,6 +11,8 @@ sixth edition, for PDF version 1.7, dated November 2006.
 '''
 
 from __future__ import generators
+import bisect
+import itertools
 
 try:
     set
@@ -21,245 +23,167 @@ import re
 import pdferrors
 from pdfobjects import PdfString, PdfObject
 
-class _PrimitiveTokens(object):
+class TokenGroup(object):
 
     # Table 3.1, page 50 of reference, defines whitespace
-    whitespaceset = set('\x00\t\n\f\r ')
-
+    eol = '\n\r'
+    whitespace = '\x00 \t\f' + eol
 
     # Text on page 50 defines delimiter characters
-    delimiterset = set('()<>{}[]/%')
+    # Escape the ]
+    delimiters = r'()<>{}[\]/%'
 
-    # Coalesce contiguous whitespace into a single token
-    whitespace_pattern = '[%s]+' % ''.join(whitespaceset)
+    # ORDER MATTERS!!!
+    special = (eol, delimiters, whitespace)
 
-    # In addition to the delimiters, we also use '\', which
-    # is special in some contexts in PDF.
-    delimiter_pattern = '\\\\|\\' + '|\\'.join(delimiterset)
+    # "normal" stuff is all but delimiters.  Also, we include
+    # escaped delimiters, but we don't include escaped whitespace,
+    # since we will later do a regular string split, and that wouldn't
+    # know about the escape.  Finally, we either do full lines,
+    # or stop at the end of the line, because of the ambiguity
+    # between comments and nested parentheses in literal strings.
+    # If we're not doing a full line, we might do the comment
+    # or string at the start, too.
+    p_normal_single = r'(?:[(%%]?[^\\%s%s]+|\\[^%s])+' % special
+    p_normal_multiple = r'[%s](?:[^\\%s]+|\\[^%s])*' % special
 
-    # Dictionary delimiters are '<<' and '>>'.  Look for
-    # these before the single variety.
-    dictdelim_pattern = r'\<\<|\>\>'
+    # A hex string.  This one's easy.
+    p_hex_string = r'\<[%s0-9A-Fa-f]*\>' % whitespace
 
-    pattern = '|'.join([whitespace_pattern,
-                        dictdelim_pattern, delimiter_pattern])
-    re_func = re.compile(pattern).finditer
-    del whitespace_pattern, dictdelim_pattern
-    del delimiter_pattern, pattern
+    p_dictdelim = r'\<\<|\>\>'
+    p_name = r'/[^%s%s]*' % (delimiters, whitespace)
 
-    def __init__(self, fdata):
+    pattern = '|'.join([p_name, p_hex_string, p_normal_single, p_normal_multiple, p_dictdelim, '.'])
+    findall = re.compile(pattern).findall
 
-        class MyIterator(object):
-            def next():
-                if not tokens:
-                    startloc = next_startloc[0]
-                    for match in next_match[0]:
-                        start = match.start()
-                        end = match.end()
-                        tappend(fdata[start:end])
-                        if start > startloc:
-                            tappend(fdata[startloc:start])
-                        next_startloc[0] = end
+    ending_pattern = r'[%s]+\>\>[%s]*stream[%s]+' % (whitespace, whitespace, eol)
+    search = re.compile(ending_pattern).search
+
+    # For splitting out simple tokens from whitespace
+    split = re.compile('([^%s]+)' % whitespace).split
+
+    def getraw(s, start, search=search, findall=findall):
+        end = search(s, start)
+        if end is None:
+            end = ()
+        else:
+            end = end.end(),
+        result = findall(s, start, *end)
+        result.reverse()
+        return result
+
+    @staticmethod
+    def gettoks(source, start, strip_comments, getraw=getraw,
+                       split=split, delimiters=delimiters, eol = eol,
+                       PdfString=PdfString, PdfObject=PdfObject, len=len, join=''.join):
+        raw = getraw(source,start)
+        pop = raw.pop
+        loc = start
+        while raw:
+            token = pop()
+            firstch = token[0]
+            if firstch not in delimiters:
+                toklist = split(token)
+                toklist.reverse()
+                lpop = toklist.pop
+                loc += len(lpop())
+                while toklist:
+                    token = lpop()
+                    loc += len(token)
+                    yield loc, PdfObject(token)
+                    loc += len(lpop())
+                continue
+
+            if firstch == '/':
+                if '#' not in token:
+                    token = PdfObject(token)
+                else:
+                    substrs = token.split('#')
+                    substrs.reverse()
+                    tokens = [substrs.pop()]
+                    while substrs:
+                        s = substrs.pop()
+                        tokens.append(chr(int(s[:2], 16)))
+                        try:
+                            tokens.append(chr(int(s[:2], 16)))
+                        except ValueError:
+                            raise pdferrors.PdfInvalidCharacterError(source, loc, s[:2])
+                        tokens.append(s[2:])
+                    result = PdfObject(join(tokens))
+                    result.encoded = token
+                    token = result
+            elif firstch == '<':
+                token = PdfString(token)
+            elif firstch == '(':
+                toklist = [token]
+                lappend = toklist.append
+                nest = 1
+                while 1:
+                    while nest and raw:
+                        token = pop()
+                        lappend(token)
+                        firstch = token[0]
+                        nest += (firstch == '(') - (firstch == ')')
+                    if not nest:
                         break
-                    else:
-                        s = fdata[startloc:]
-                        next_startloc[0] = len(fdata)
-                        if s:
-                            tappend(s)
-                    if not tokens:
-                        raise StopIteration
-                return tpop()
-            next = staticmethod(next)
+                    if not raw:
+                        raw = getraw(source,start)
+                        pop = raw.pop
+                        if not raw:
+                            # Error here, maybe???
+                            break
+                token = PdfString(join(toklist))
+            elif firstch == '%':
+                toklist = [token]
+                while raw[-1] not in eol:
+                    toklist.append(pop())
+                token = join(toklist)
+                if strip_comments:
+                    loc += len(token)
+                    continue
 
-        def coalesce(result):
-            ''' This function coalesces tokens together up until
-                the next delimiter or whitespace.
-                All of the coalesced tokens will either be non-matches,
-                or will be a matched backslash.  We distinguish the
-                non-matches by the fact that next() will have left
-                a following match inside self.tokens for the actual match.
-            '''
-            # Optimized path for usual case -- regular data (not a name string),
-            # with no escape character, and followed by whitespace.
-
-            if tokens:
-                token = tpop()
-                if token != '\\':
-                    if token[0] not in whitespace:
-                        tappend(token)
-                    return
-                result.append(token)
-
-            # Non-optimized path.  Either start of a name string received,
-            # or we just had one escape.
-
-            rappend = result.append
-            for token in self:
-                if tokens:
-                    rappend(token)
-                    token = tpop()
-                if token != '\\':
-                    if token[0] not in whitespace:
-                        tappend(token)
-                    return
-                rappend(token)
-
-        def setstart(startloc):
-            while tokens:
-                tpop()
-            old_loc = next_startloc[0]
-            if old_loc != startloc:
-                next_startloc[0] = startloc
-                next_match[0] = re_func(fdata, startloc)
-
-        self.fdata = fdata
-        self.tokens = tokens = []
-        self.iterator = iterator = MyIterator()
-        self.next = iterator.next
-        self.next_match = next_match = [None]
-        self.startloc = next_startloc = [0]
-        self.coalesce = coalesce
-        self.setstart = setstart
-        tappend = tokens.append
-        tpop = tokens.pop
-        re_func = self.re_func
-        whitespace = self.whitespaceset
-
-    def __iter__(self):
-        return self.iterator
-
-    def floc(self):
-        return self.startloc[0] - sum([len(x) for x in self.tokens])
+            loc += len(token)
+            yield loc, token
 
 class PdfTokens(object):
 
     def __init__(self, fdata, startloc=0, strip_comments=True):
-
-        def comment(token):
-            tokens = [token]
-            for token in primitive:
-                tokens.append(token)
-                if token[0] in whitespaceset and ('\n' in token or '\r' in token):
-                    break
-            return not self.strip_comments and join(tokens)
-
-        def single(token):
-            return token
-
-        def regular_string(token):
-            def escaped():
-                escaped = False
-                i = -2
-                while tokens[i] == '\\':
-                    escaped = not escaped
-                    i -= 1
-                return escaped
-
-            tokens = [token]
-            nestlevel = 1
-            for token in primitive:
-                tokens.append(token)
-                if token in '()' and not escaped():
-                    nestlevel += token == '(' or -1
-                    if not nestlevel:
-                        break
-            else:
-                raise pdferrors.PdfUnexpectedEOFError(self.fdata)
-            return PdfString(join(tokens))
-
-        def hex_string(token):
-            tokens = [token]
-            for token in primitive:
-                tokens.append(token)
-                if token == '>':
-                    break
-            while tokens[-2] == '>>':
-                primitive.tokens.append(tokens.pop(-2))
-            return PdfString(join(tokens))
-
-        def normal_data(token):
-
-            # Obscure optimization -- we can get here with
-            # whitespace or regular character data.  If we get
-            # here with whitespace, then there won't be an additional
-            # token queued up in the primitive object, otherwise there
-            # will...
-            if primitive_tokens:     #if token[0] not in whitespaceset:
-                tokens = [token]
-                coalesce(tokens)
-                return PdfObj(join(tokens))
-
-        def name_string(token):
-            tokens = [token]
-            coalesce(tokens)
-            token = join(tokens)
-            if '#' not in token:
-                return PdfObj(token)
-            substrs = token.split('#')
-            substrs.reverse()
-            tokens = [substrs.pop()]
-            while substrs:
-                s = substrs.pop()
-                tokens.append(chr(int(s[:2], 16)))
-                try:
-                    tokens.append(chr(int(s[:2], 16)))
-                except ValueError:
-                    raise pdferrors.PdfInvalidCharacterError(self.fdata, self.floc, s[:2])
-                tokens.append(s[2:])
-            result = PdfObj(join(tokens))
-            result.encoded = token
-            return result
-
-        def broken(token):
-            raise pdferrors.PdfUnexpectedTokenError(self.fdata, self.floc, token)
-
-        dispatch = {
-            '(': regular_string,
-            ')': broken,
-            '<': hex_string,
-            '>': broken,
-            '[': single,
-            ']': single,
-            '{': single,
-            '}': single,
-            '/': name_string,
-            '%' : comment,
-            '<<': single,
-            '>>': single,
-        }.get
-
-        class MyIterator(object):
-            def next():
-                while not tokens:
-                    token = primitive_next()
-                    token = dispatch(token, normal_data)(token)
-                    if token:
-                        return token
-                return tokens.pop()
-            next = staticmethod(next)
-
-        self.primitive = primitive = _PrimitiveTokens(fdata)
-        self.setstart = primitive.setstart
-        primitive.setstart(startloc)
         self.fdata = fdata
         self.strip_comments = strip_comments
-        self.tokens = tokens = []
-        self.iterator = iterator = MyIterator()
+        self.tokens = []
+        self.current = [(None, None)]
+        self.setstart(startloc)
+
+    def setstart(self, startloc, gettoks=TokenGroup.gettoks, bisect=bisect.bisect_left, len=len, islice=itertools.islice):
+        current = self.current
+        tokens = self.tokens
+
+        ok = tokens and tokens[0][0] - len(tokens[0][1]) <= startloc < tokens[-1][0]
+        if not ok:
+            tokens[:] = gettoks(self.fdata, startloc, self.strip_comments)
+
+
+        def iterator():
+            start = bisect(tokens, (startloc+1,))
+            itokens = start > 0 and islice(tokens, start, None) or tokens
+            while 1:
+                for token in itokens:
+                    current[0] = token
+                    yield token[1]
+                tokens[:] = gettoks(self.fdata, current[0][0], self.strip_comments)
+                if not tokens:
+                    raise StopIteration
+                itokens = tokens
+        iterator = iterator()
+        self.iterator = iterator
         self.next = iterator.next
-        primitive_next = primitive.next
-        primitive_tokens = primitive.tokens
-        whitespaceset = primitive.whitespaceset
-        coalesce = primitive.coalesce
-        PdfObj = PdfObject
-        join = ''.join
 
     def floc(self):
-        return self.primitive.floc() - sum([len(x) for x in self.tokens])
+        return self.current[0][0]
     floc = property(floc)
 
     def __iter__(self):
         return self.iterator
 
-    def multiple(self, count):
-        next = self.next
-        return [next() for i in range(count)]
+    def multiple(self, count, islice=itertools.islice, list=list):
+        return list(islice(self, count))
