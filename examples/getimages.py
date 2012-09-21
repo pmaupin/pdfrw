0b3666c31a6f64798f108cc66095ab6d6c63e2fa
pmaupin <pmaupin@bf5f1c66-d648-11de-a63d-2763023b5f98>
2012-09-21 01:45:37 +0000

    Simple example to find images inside PDF

---
+++ examples/getimages.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+'''
+usage:   getimages.py <some.pdf>
+
+Locates images and image type within the PDF.
+
+Need to add code to write them out.....
+
+'''
+
+import sys
+import os
+
+import find_pdfrw
+from pdfrw import PdfReader, PdfDict, PdfArray, PdfName
+
+
+XObject = PdfName.XObject
+Image = PdfName.Image
+
+def find_images(obj, visited=set()):
+    if not isinstance(obj, (PdfDict, PdfArray)):
+        return
+
+    # Don't get stuck in an infinite loop
+    myid = id(obj)
+    if myid in visited:
+        return
+    visited.add(myid)
+
+    if isinstance(obj, PdfDict):
+        if obj.Type == XObject and obj.Subtype == Image:
+            yield obj
+        obj = obj.itervalues()
+
+    for item in obj:
+        for result in find_images(item, visited):
+            yield result
+
+def show_image(image):
+    print '******************'
+    print
+    print image
+    print repr(image.stream[:200])
+    print
+    try:
+        print image.Filter
+        print int(image.Height)
+        print int(image.Width)
+        print int(image.Length)
+        print image.ColorSpace
+        print int(image.DecodeParms.Columns)
+        print int(image.DecodeParms.K)
+        print int(image.DecodeParms.Rows)
+    except:
+        pass
+
+if __name__ == '__main__':
+    inpfn, = sys.argv[1:]
+    for image in find_images(PdfReader(inpfn)):
+        show_image(image)
