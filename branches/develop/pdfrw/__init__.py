844bad3274ab75758789366361995e7714267edc
pmaupin <pmaupin@bf5f1c66-d648-11de-a63d-2763023b5f98>
2012-11-25 14:36:41 +0000

    updated develop branch to match trunk

--- branches/develop/pdfrw/__init__.py
+++ branches/develop/pdfrw/__init__.py
@@ -1,6 +1,8 @@
 # A part of pdfrw (pdfrw.googlecode.com)
-# Copyright (C) 2006-2009 Patrick Maupin, Austin, Texas
+# Copyright (C) 2006-2012 Patrick Maupin, Austin, Texas
 # MIT license -- See LICENSE.txt for details
+
+__version__ = '0.1'
 
 from pdfrw.pdfwriter import PdfWriter
 from pdfrw.pdfreader import PdfReader
