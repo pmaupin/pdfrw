b0b8ca5f29d9285d7095ea917d1c4ecd8c1739fc
ןנָתָנָוֹי <jdellagostin@gmail.com>
2016-12-05 09:40:09 -0200

    Fixing PEP8 related compliant issues.

--- examples/subset_booklets.py
+++ examples/subset_booklets.py
@@ -23,39 +23,39 @@ import time
 from pdfrw import PdfReader, PdfWriter, PageMerge
 
 BOOKLET_SIZE = 20
-start = time.time()
+START = time.time()
 
 def fixpage(*pages):
     result = PageMerge() + (x for x in pages if x is not None)
     result[-1].x += result[0].w
     return result.render()
 
-inpfn, = sys.argv[1:]
-outfn = 'booklet.' + os.path.basename(inpfn)
-allipages = PdfReader(inpfn).pages
-print 'The pdf file '+str(inpfn)+' has '+str(len(allipages))+' pages.'
+INPFN, = sys.argv[1:]
+OUTFN = 'booklet.' + os.path.basename(INPFN)
+ALL_IPAGES = PdfReader(INPFN).pages
+print 'The pdf file '+str(INPFN)+' has '+str(len(ALL_IPAGES))+' pages.'
 
 #Make sure we have an even number
-if len(allipages) & 1:
-   allipages.append(None)
-   print 'Inserting one more blank page to make pages number even.'
-num_of_iter, iters_left = divmod(len(allipages), BOOKLET_SIZE)
+if len(ALL_IPAGES) & 1:
+    ALL_IPAGES.append(None)
+    print 'Inserting one more blank page to make pages number even.'
+NUM_OF_ITER, ITERS_LEFT = divmod(len(ALL_IPAGES), BOOKLET_SIZE)
 
-print 'Making '+str(num_of_iter)+' subbooklets of '+str(BOOKLET_SIZE)+' pages each.'
+print 'Making '+str(NUM_OF_ITER)+' subbooklets of '+str(BOOKLET_SIZE)+' pages each.'
 opages = []
-for iteration in range(0,num_of_iter):
-    ipages = allipages[iteration*BOOKLET_SIZE:(iteration+1)*BOOKLET_SIZE]
+for iteration in range(0, NUM_OF_ITER):
+    ipages = ALL_IPAGES[iteration*BOOKLET_SIZE:(iteration+1)*BOOKLET_SIZE]
     while len(ipages) > 2:
         opages.append(fixpage(ipages.pop(), ipages.pop(0)))
         opages.append(fixpage(ipages.pop(0), ipages.pop()))
 
 # Making one more subbooklet with the left pages
-ipages = allipages[len(allipages)-iters_left:len(allipages)]
+ipages = ALL_IPAGES[len(ALL_IPAGES)-ITERS_LEFT:len(ALL_IPAGES)]
 while len(ipages) > 2:
     opages.append(fixpage(ipages.pop(), ipages.pop(0)))
     opages.append(fixpage(ipages.pop(0), ipages.pop()))
-if len(ipages) >=1:
+if len(ipages) >= 1:
     opages.append(fixpage(ipages.pop(), ipages.pop(0)))
 
-PdfWriter().addpages(opages).write(outfn)
-print 'It took '+ str(round(time.time()-start,2))+' seconds to make the pdf subbooklets changes.' 
+PdfWriter().addpages(opages).write(OUTFN)
+print 'It took '+ str(round(time.time()-START, 2))+' seconds to make the pdf subbooklets changes.'
