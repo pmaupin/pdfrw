3c933fb688d49cc926f98ac2676189f767faca6b
Haochen Wu <github@haochenwu.com>
2017-02-25 16:44:14 -0800

    Add a new padding option to booklet

--- examples/booklet.py
+++ examples/booklet.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 '''
-usage:   booklet.py my.pdf
+usage:   booklet.py [-p] my.pdf
 
 Creates booklet.my.pdf
 
 Pages organized in a form suitable for booklet printing, e.g.
 to print 4 8.5x11 pages using a single 11x17 sheet (double-sided).
+
+The output would be using the same type of sheet
+and you can get up to 3 blank sides if -p is enabled.
+
+Otherwise the two sides in the middle will be in original page size
+and you can have 1 blank sides at most.
+
 '''
 
-import sys
 import os
+import argparse
 
 from pdfrw import PdfReader, PdfWriter, PageMerge
 
@@ -21,13 +28,23 @@ def fixpage(*pages):
     return result.render()
 
 
-inpfn, = sys.argv[1:]
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="Input pdf file name")
+parser.add_argument("-p", "--padding", action = "store_true",
+                    help="Padding the document so that all pages use the same type of sheet")
+args = parser.parse_args()
+
+inpfn = args.input
 outfn = 'booklet.' + os.path.basename(inpfn)
 ipages = PdfReader(inpfn).pages
 
-# Make sure we have an even number of sides
-# Each sheet contains 2 sides and 4 original pages in total
-ipages += [None]*(-len(ipages)%4)
+if args.padding:
+    pad_to = 4
+else:
+    pad_to = 2
+
+# Make sure we have a correct number of sides
+ipages += [None]*(-len(ipages)%pad_to)
 
 opages = []
 while len(ipages) > 2:
