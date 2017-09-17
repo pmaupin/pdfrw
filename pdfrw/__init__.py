77d3814f33ae8490961d3fd576557666a77e04c2
Patrick Maupin <pmaupin@gmail.com>
2017-09-17 13:49:02 -0500

    Release 0.4

--- pdfrw/__init__.py
+++ pdfrw/__init__.py
@@ -10,7 +10,7 @@ from .tokens import PdfTokens
 from .errors import PdfParseError
 from .pagemerge import PageMerge
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 # Add a tiny bit of compatibility to pyPdf
 
