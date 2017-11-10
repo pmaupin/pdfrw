ee9bb99b5e26527c138f868a2ae23aefe74a276a
Henddher Pedroza <hpedroza@wje.com>
2017-11-10 11:34:51 -0600

    Can't use print in python 3

--- tests/test_flate_png.py
+++ tests/test_flate_png.py
@@ -32,72 +32,57 @@ class TestFlatePNG(unittest.TestCase):
     def test_flate_png(self):
         b64 = 'AAAAAAD//wACAAA2AAAAAQAADwAAAgEAACcAAQL/AAAzAP8AAgAANgACAAEAAO8AAAABAAF1AAAAAgAANgADAAEAAfsAAAACAAA2AAQCAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAQECBXx8AAIAAAGHAAAAAgAANgAMAAEDCcMAAAACAAA2AA0CAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQABBxI2AAAEAfn5AAAWAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQIAAAAAAAEAAQ6fJgAAAAIAADYAHwIAAAAAAAECAAAAAAABAgAAAAAAAQIAAAAAAAECAAAAAAABAgAAAAAAAQABESDsAAAAAgAANgAmAAAAAAD//wIAAAAAAAACARp0hgEBAgAA/eAAAA=='
         predictor, columns, colors, bpc = (12, 6, 1, 8)
-        print predictor, columns, colors, bpc
 
         data = base64.b64decode(b64)
-        print len(data)
         d1, error1 = flate_png_orig(data, predictor, columns, colors, bpc)
-        print error1
 
         data = base64.b64decode(b64)
-        print len(data)
         d2, error2 = flate_png(data, predictor, columns, colors, bpc)
-        print error2
 
         assert d1 == d2 
 
     def test_flate_png_filter_0(self):
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4)
         d1, error1 = flate_png_orig(data, 12, nc, ncolors, bpc) 
-        print error1
 
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4)
         d2, error2 = flate_png(data, 12, nc, ncolors, bpc)
-        print error2
 
         assert d1 == d2 
 
     def test_flate_png_filter_1(self):
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=1)
         d1, error1 = flate_png_orig(data, 12, nc, ncolors, bpc) 
-        print error1
 
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=1)
         d2, error2 = flate_png(data, 12, nc, ncolors, bpc)
-        print error2
 
         assert d1 == d2 
 
     def test_flate_png_filter_2(self):
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=2)
         d1, error1 = flate_png_orig(data, 12, nc, ncolors, bpc) 
-        print error1
 
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=2)
         d2, error2 = flate_png(data, 12, nc, ncolors, bpc)
-        print error2
 
         assert d1 == d2 
 
     def test_flate_png_filter_3(self):
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=3)
         d1, error1 = flate_png_orig(data, 12, nc, ncolors, bpc) 
-        print error1
 
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=3)
         d2, error2 = flate_png(data, 12, nc, ncolors, bpc)
-        print error2
 
         assert d1 == d2 
 
     def test_flate_png_filter_4(self):
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=4)
         d1, error1 = flate_png_orig(data, 12, nc, ncolors, bpc) 
-        print error1
 
         data, nc, nr, bpc, ncolors = create_data(nc=4, nr=6, bpc=8, ncolors=4, filter_type=4)
         d2, error2 = flate_png(data, 12, nc, ncolors, bpc)
-        print error2
 
         assert d1 == d2 
 
